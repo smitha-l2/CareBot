3ecad1ab98a28f632ec340b7984f3535ebdc4c20
rajesh.davuluri@cognizant.com <rajesh.davuluri@cognizant.com>
2025-08-05 19:59:45 +0530

    Added new feture

--- start-backend-simple.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "c:\Users\2313829\Downloads\Carebot\java-carebot-backend"
-echo Starting CareBot Backend with Simple Profile...
-echo Backend will be available at: http://localhost:8080
-echo Press Ctrl+C to stop the server
-echo.
-java "-Dspring.profiles.active=simple" -jar target\java-carebot-backend-1.0.0.jar
-pause
